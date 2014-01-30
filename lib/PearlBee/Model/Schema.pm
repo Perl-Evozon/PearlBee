4f084cf49957d84f55d9af0c3336cd8aa112cccd
Andrei Cacio <andrei.cacio@evozon.com>
2014-01-29 20:56:26 -0800

    Removed the old Schema

--- lib/PearlBee/Model/Schema.pm
+++ lib/PearlBee/Model/Schema.pm
@@ -11,8 +11,8 @@ use base 'DBIx::Class::Schema';
 __PACKAGE__->load_namespaces;
 
 
-# Created by DBIx::Class::Schema::Loader v0.07010 @ 2014-01-08 20:33:25
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:lzNZWO9wFANW+muwHGCL9Q
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2014-01-29 20:14:37
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Zq9vNb/hfjZ6QMLJYWqjAQ
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration

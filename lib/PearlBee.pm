3e475d83b2678901b24cec8dbaf0363414ed66f9
Andrei Cacio <andrei.cacio@evozon.com>
2014-02-04 19:38:19 -0800

    Added a new Email template for new comments submited on a post. When a comment is submited, the author gest notified via email

--- lib/PearlBee.pm
+++ lib/PearlBee.pm
@@ -152,9 +152,11 @@ post '/comment/add' => sub {
   my $email    = params->{email};
   my $text     = params->{comment};
   my $post_id  = params->{id};
-  my $secret    = params->{secret};
+  my $secret   = params->{secret};
+  my $post     = resultset('Post')->find( $post_id );
 
   if ( md5_hex($secret) eq session('secret') ) {
+    # The user entered the correct secrete code
     eval {
       my $comment = resultset('Comment')->create({
           fullname => $fullname,
@@ -162,7 +164,22 @@ post '/comment/add' => sub {
           email    => $email,
           post_id   => $post_id
         });
-      
+
+      # Notify the author that a new comment was submited
+      my $author = $post->user;
+      Email::Template->send( config->{email_templates} . 'new_comment.tt',
+      {
+          From    => 'no-reply@PearlBee.com',
+          To      => $author->email,
+          Subject => 'A new comment was submited to your post',
+
+          tt_vars => { 
+              fullname => $fullname,
+              title    => $post->title,
+              post_url => config->{app_url} . '/post/' . $post->id,
+              url      => config->{app_url}
+          },
+      }) or error "Could not send the email";
     };
 
     error $@ if ( $@ );
@@ -171,7 +188,7 @@ post '/comment/add' => sub {
   else {
     # The secret code inncorrect
     # Repopulate the fields with the data
-    my $post     = resultset('Post')->find( $post_id );
+    
     my @categories   = resultset('Category')->all();
 
     # Grap the approved comments for this post
@@ -201,13 +218,13 @@ post '/comment/add' => sub {
 
     template 'post', 
       { 
-        post     => $post, 
-        categories   => \@categories, 
-        comments   => \@comments,
-        fullname   => $fullname,
-        email     => $email,
-        text     => $text,
-        warning    => 'Wrong secret code. Please enter the code again'
+        post        => $post, 
+        categories  => \@categories, 
+        comments    => \@comments,
+        fullname    => $fullname,
+        email       => $email,
+        text        => $text,
+        warning     => 'Wrong secret code. Please enter the code again'
       }, 
       { layout => 'main' };
   }  
@@ -223,30 +240,30 @@ List all posts by selected category
 get '/posts/category/:slug' => sub {
 
   my $nr_of_rows  = 5; # Number of posts per page
-  my $slug     = params->{slug};
-  my @posts     = resultset('Post')->search({ 'category.slug' => $slug, 'status' => 'published' }, { join => { 'post_categories' => 'category' }, rows => $nr_of_rows });
+  my $slug        = params->{slug};
+  my @posts       = resultset('Post')->search({ 'category.slug' => $slug, 'status' => 'published' }, { join => { 'post_categories' => 'category' }, rows => $nr_of_rows });
   my $nr_of_posts = resultset('Post')->search({ 'category.slug' => $slug }, { join => { 'post_categories' => 'category' } })->count;
-  my @tags      = resultset('View::PublishedTags')->all();
-  my @categories   = resultset('View::PublishedCategories')->search({ name => { '!=' => 'Uncategorized'} });
-  my @recent     = resultset('Post')->search({ status => 'published' },{ order_by => "created_date DESC", rows => 3 });
-  my @popular   = resultset('View::PopularPosts')->search({}, { rows => 3 });
+  my @tags        = resultset('View::PublishedTags')->all();
+  my @categories  = resultset('View::PublishedCategories')->search({ name => { '!=' => 'Uncategorized'} });
+  my @recent      = resultset('Post')->search({ status => 'published' },{ order_by => "created_date DESC", rows => 3 });
+  my @popular     = resultset('View::PopularPosts')->search({}, { rows => 3 });
 
-  my $total_pages = ( ($nr_of_posts / $nr_of_rows) != int($nr_of_posts / $nr_of_rows) ) ? int($nr_of_posts / $nr_of_rows) + 1 : ($nr_of_posts % $nr_of_rows);
+  my $total_pages   = ( ($nr_of_posts / $nr_of_rows) != int($nr_of_posts / $nr_of_rows) ) ? int($nr_of_posts / $nr_of_rows) + 1 : ($nr_of_posts % $nr_of_rows);
   my $previous_link = '#';
   my $next_link     = ( $total_pages < 2 ) ? '#' : '/posts/category/' . $slug . '/page/2';
 
   # Extract all posts with the wanted category
   template 'index', 
       { 
-        posts       => \@posts,
-        recent       => \@recent,
-        popular     => \@popular,
-        tags        => \@tags,
-        page       => 1,
-        categories     => \@categories,
+        posts         => \@posts,
+        recent        => \@recent,
+        popular       => \@popular,
+        tags          => \@tags,
+        page          => 1,
+        categories    => \@categories,
         total_pages   => $total_pages,
         next_link     => $next_link,
-        previous_link   => $previous_link
+        previous_link => $previous_link
     }, 
     { layout => 'main' };
 };
@@ -260,14 +277,14 @@ List all posts by selected category
 get '/posts/category/:slug/page/:page' => sub {
 
   my $nr_of_rows  = 5; # Number of posts per page
-  my $page     = params->{page};
-  my $slug     = params->{slug};
-  my @posts     = resultset('Post')->search({ 'category.slug' => $slug, 'status' => 'published' }, { join => { 'post_categories' => 'category' }, rows => $nr_of_rows, page => $page });
+  my $page        = params->{page};
+  my $slug        = params->{slug};
+  my @posts       = resultset('Post')->search({ 'category.slug' => $slug, 'status' => 'published' }, { join => { 'post_categories' => 'category' }, rows => $nr_of_rows, page => $page });
   my $nr_of_posts = resultset('Post')->search({ 'category.slug' => $slug }, { join => { 'post_categories' => 'category' } })->count;
-  my @tags      = resultset('View::PublishedTags')->all();
-  my @categories   = resultset('View::PublishedCategories')->search({ name => { '!=' => 'Uncategorized'} });
-  my @recent     = resultset('Post')->search({ status => 'published' },{ order_by => "created_date DESC", rows => 3 });
-  my @popular   = resultset('View::PopularPosts')->search({}, { rows => 3 });
+  my @tags        = resultset('View::PublishedTags')->all();
+  my @categories  = resultset('View::PublishedCategories')->search({ name => { '!=' => 'Uncategorized'} });
+  my @recent      = resultset('Post')->search({ status => 'published' },{ order_by => "created_date DESC", rows => 3 });
+  my @popular     = resultset('View::PopularPosts')->search({}, { rows => 3 });
 
   my $total_pages   = ( ($nr_of_posts / $nr_of_rows) != int($nr_of_posts / $nr_of_rows) ) ? int($nr_of_posts / $nr_of_rows) + 1 : ($nr_of_posts % $nr_of_rows);
 
@@ -277,15 +294,15 @@ get '/posts/category/:slug/page/:page' => sub {
 
   template 'index', 
       { 
-        posts       => \@posts,
-        recent       => \@recent,
-        popular     => \@popular,
-        tags        => \@tags,
-        categories     => \@categories,
-        page       => $page,
+        posts         => \@posts,
+        recent        => \@recent,
+        popular       => \@popular,
+        tags          => \@tags,
+        categories    => \@categories,
+        page          => $page,
         total_pages   => $total_pages,
         next_link     => $next_link,
-        previous_link   => $previous_link
+        previous_link => $previous_link
     }, 
     { layout => 'main' };
 };
@@ -299,13 +316,13 @@ List all posts by selected tag
 get '/posts/tag/:slug' => sub {
 
   my $nr_of_rows  = 5; # Number of posts per page
-  my $slug     = params->{slug};
-  my @posts     = resultset('Post')->search({ 'tag.slug' => $slug, 'status' => 'published' }, { join => { 'post_tags' => 'tag' }, rows => $nr_of_rows });
+  my $slug        = params->{slug};
+  my @posts       = resultset('Post')->search({ 'tag.slug' => $slug, 'status' => 'published' }, { join => { 'post_tags' => 'tag' }, rows => $nr_of_rows });
   my $nr_of_posts = resultset('Post')->search({ 'tag.slug' => $slug }, { join => { 'post_tags' => 'tag' } })->count;
-  my @tags         = resultset('View::PublishedTags')->all();
-  my @categories   = resultset('View::PublishedCategories')->search({ name => { '!=' => 'Uncategorized'} });
-  my @recent     = resultset('Post')->search({ status => 'published' },{ order_by => "created_date DESC", rows => 3 });
-  my @popular   = resultset('View::PopularPosts')->search({}, { rows => 3 });
+  my @tags        = resultset('View::PublishedTags')->all();
+  my @categories  = resultset('View::PublishedCategories')->search({ name => { '!=' => 'Uncategorized'} });
+  my @recent      = resultset('Post')->search({ status => 'published' },{ order_by => "created_date DESC", rows => 3 });
+  my @popular     = resultset('View::PopularPosts')->search({}, { rows => 3 });
 
   my $total_pages = ( ($nr_of_posts / $nr_of_rows) != int($nr_of_posts / $nr_of_rows) ) ? int($nr_of_posts / $nr_of_rows) + 1 : ($nr_of_posts % $nr_of_rows);
   my $previous_link = '#';
@@ -313,15 +330,15 @@ get '/posts/tag/:slug' => sub {
 
   template 'index', 
       {         
-        posts       => \@posts,
-        recent       => \@recent,
-        popular     => \@popular,
-        tags        => \@tags,
-        page       => 1,
-        categories     => \@categories,
+        posts         => \@posts,
+        recent        => \@recent,
+        popular       => \@popular,
+        tags          => \@tags,
+        page          => 1,
+        categories    => \@categories,
         total_pages   => $total_pages,
         next_link     => $next_link,
-        previous_link   => $previous_link
+        previous_link => $previous_link
     }, 
     { layout => 'main' };
 };
@@ -335,33 +352,33 @@ List all posts by selected tag
 get '/posts/tag/:slug/page/:page' => sub {
 
   my $nr_of_rows  = 5; # Number of posts per page
-  my $page     = params->{page};
-  my $slug     = params->{slug};
-  my $tag     = resultset('Tag')->find({ slug => $slug });
-  my @posts     = resultset('Post')->search({ 'tag.slug' => $slug, 'status' => 'published' }, { join => { 'post_tags' => 'tag' }, rows => $nr_of_rows });
+  my $page        = params->{page};
+  my $slug        = params->{slug};
+  my $tag         = resultset('Tag')->find({ slug => $slug });
+  my @posts       = resultset('Post')->search({ 'tag.slug' => $slug, 'status' => 'published' }, { join => { 'post_tags' => 'tag' }, rows => $nr_of_rows });
   my $nr_of_posts = resultset('Post')->search({ 'tag.slug' => $slug }, { join => { 'post_tags' => 'tag' } })->count;
-  my @tags         = resultset('View::PublishedTags')->all();
-  my @categories   = resultset('View::PublishedCategories')->search({ name => { '!=' => 'Uncategorized'} });
-  my @recent     = resultset('Post')->search({ status => 'published' },{ order_by => "created_date DESC", rows => 3 });
-  my @popular   = resultset('View::PopularPosts')->search({}, { rows => 3 });
+  my @tags        = resultset('View::PublishedTags')->all();
+  my @categories  = resultset('View::PublishedCategories')->search({ name => { '!=' => 'Uncategorized'} });
+  my @recent      = resultset('Post')->search({ status => 'published' },{ order_by => "created_date DESC", rows => 3 });
+  my @popular     = resultset('View::PopularPosts')->search({}, { rows => 3 });
 
   my $total_pages   = ( ($nr_of_posts / $nr_of_rows) != int($nr_of_posts / $nr_of_rows) ) ? int($nr_of_posts / $nr_of_rows) + 1 : ($nr_of_posts % $nr_of_rows);
 
   # Calculate the next and previous page link
-  my $previous_link   = ( $page == 1 ) ? '#' : '/posts/tag/' . $slug . '/page/' . ( int($page) - 1 );
+  my $previous_link = ( $page == 1 ) ? '#' : '/posts/tag/' . $slug . '/page/' . ( int($page) - 1 );
   my $next_link     = ( $page == $total_pages ) ? '#' : '/posts/tag/' . $slug . '/page/' . ( int($page) + 1 );
 
   template 'index', 
       { 
-        posts       => \@posts,
-        recent       => \@recent,
-        popular     => \@popular,
-        tags        => \@tags,
-        page       => $page,
-        categories     => \@categories,
+        posts         => \@posts,
+        recent        => \@recent,
+        popular       => \@popular,
+        tags          => \@tags,
+        page          => $page,
+        categories    => \@categories,
         total_pages   => $total_pages,
         next_link     => $next_link,
-        previous_link   => $previous_link
+        previous_link => $previous_link
     }, 
     { layout => 'main' };
 };

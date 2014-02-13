5b44201762c401f5e2d08a1e4026076eb4fb1244
Andrei Cacio <andrei.cacio@evozon.com>
2014-02-12 16:21:48 -0800

    Regenerated the ORM layer

--- lib/PearlBee/Model/Schema/Result/Setting.pm
+++ lib/PearlBee/Model/Schema/Result/Setting.pm
@@ -19,11 +19,11 @@ __PACKAGE__->table("settings");
 
 =head1 ACCESSORS
 
-=head2 timezone_id
+=head2 timezone
 
-  data_type: 'integer'
-  is_foreign_key: 1
+  data_type: 'varchar'
   is_nullable: 0
+  size: 255
 
 =head2 social_media
 
@@ -41,35 +41,18 @@ __PACKAGE__->table("settings");
 =cut
 
 __PACKAGE__->add_columns(
-  "timezone_id",
-  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "timezone",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
   "social_media",
   { data_type => "tinyint", default_value => 1, is_nullable => 0 },
   "blog_path",
   { data_type => "varchar", default_value => "/", is_nullable => 0, size => 255 },
 );
-__PACKAGE__->set_primary_key("timezone_id", "social_media", "blog_path");
-
-=head1 RELATIONS
-
-=head2 timezone
-
-Type: belongs_to
-
-Related object: L<PearlBee::Model::Schema::Result::Timezone>
-
-=cut
-
-__PACKAGE__->belongs_to(
-  "timezone",
-  "PearlBee::Model::Schema::Result::Timezone",
-  { id => "timezone_id" },
-  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
-);
+__PACKAGE__->set_primary_key("timezone", "social_media", "blog_path");
 
 
-# Created by DBIx::Class::Schema::Loader v0.07010 @ 2014-02-08 04:37:42
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:jQSvJL6TvIjPr99+SZi5aQ
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2014-02-08 13:51:23
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:uVgszmd0Rjqg7w6BRc2d1g
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration

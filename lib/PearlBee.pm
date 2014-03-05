4be1d52a8e3e5bb4a725e91a96b375007fa39650
Andrei Cacio <andrei.cacio@evozon.com>
2014-03-05 12:59:49 -0800

    Refactored the comment add action

--- lib/PearlBee.pm
+++ lib/PearlBee.pm
@@ -6,9 +6,7 @@ use Dancer2;
 use Dancer2::Plugin::DBIC;
 
 # Other used modules
-use Authen::Captcha;
 use Digest::MD5 qw(md5_hex);
-use Gravatar::URL;
 use DateTime;
 
 # Included controllers
@@ -33,6 +31,7 @@ use PearlBee::Author::Comment;
 
 use PearlBee::Helpers::Util qw(generate_crypted_filename);
 use PearlBee::Helpers::Pagination qw(get_total_pages get_previous_next_link);
+use PearlBee::Helpers::Captcha;
 
 our $VERSION = '0.1';
 
@@ -126,27 +125,13 @@ get '/post/:slug' => sub {
   my $slug       = params->{slug};
   my $post       = resultset('Post')->find({ slug => $slug });
   my $settings   = resultset('Setting')->first;
-  my @tags        = resultset('View::PublishedTags')->all();
+  my @tags       = resultset('View::PublishedTags')->all();
   my @categories = resultset('View::PublishedCategories')->search({ name => { '!=' => 'Uncategorized'} });
-  my @recent      = resultset('Post')->search({ status => 'published' },{ order_by => { -desc => "created_date" }, rows => 3 });
-  my @popular     = resultset('View::PopularPosts')->search({}, { rows => 3 });
-
-  my $captcha    = Authen::Captcha->new();
-
-  # set the data_folder. contains flatfile db to maintain state
-  $captcha->data_folder('public/captcha');
-
-  # set directory to hold publicly accessable images
-  $captcha->output_folder('public/captcha/image');
-  my $md5sum = $captcha->generate_code(5);
-
-  # Rename the image file so that the encrypted code won't show on the UI
-  unlink config->{captcha_folder} . "/image/image.png";
-  my $command = "mv " . config->{captcha_folder} . "/image/" . $md5sum . ".png" . " " . config->{captcha_folder} . "/image/image.png";
-  `$command`;
+  my @recent     = resultset('Post')->search({ status => 'published' },{ order_by => { -desc => "created_date" }, rows => 3 });
+  my @popular    = resultset('View::PopularPosts')->search({}, { rows => 3 });
 
   # Store the encrypted code on the session
-  session secret => $md5sum;
+  session secret => PearlBee::Helpers::Captcha::generate();
 
   # Grab the approved comments for this post
   my @comments;
@@ -173,55 +158,32 @@ Add a comment method
 
 post '/comment/add' => sub {
 
-  # Set the proper timezone
-  my $dt       = DateTime->now;          
-  my $settings = resultset('Setting')->first;
-  $dt->set_time_zone( $settings->timezone );
+  my $fullname    = params->{fullname};
+  my $post_id     = params->{id};
+  my $secret      = params->{secret};
+  my @comments    = resultset('Comment')->search({ post_id => $post_id, status => 'approved' });
+  my $post        = resultset('Post')->find( $post_id );
+  my @categories  = resultset('Category')->all();
+  my @recent      = resultset('Post')->search({ status => 'published' },{ order_by => { -desc => "created_date" }, rows => 3 });
+  my @popular     = resultset('View::PopularPosts')->search({}, { rows => 3 });
+  my $params      = params;
 
-  my $fullname = params->{fullname};
-  my $email    = params->{email};
-  my $website  = params->{website} || '';
-  my $text     = params->{comment};
-  my $post_id  = params->{id};
-  my $secret   = params->{secret};
-  my @comments = resultset('Comment')->search({ post_id => $post_id, status => 'approved' });
-  my $post     = resultset('Post')->find( $post_id );
-  my @categories   = resultset('Category')->all();
-
-  # Grab the gravatar if exists, or a default image if not
-  my $gravatar = gravatar_url(email => $email);
+  my $template_params = {
+    post        => $post,
+    categories  => \@categories,
+    popular     => \@popular,
+    recent      => \@recent,
+    comments    => \@comments,
+    warning     => 'The secret code is incorrect'
+  };
 
   if ( md5_hex($secret) eq session('secret') ) {
     # The user entered the correct secrete code
     eval {
 
       # If the person who leaves the comment is either the author or the admin the comment is automaticly approved
-      my $user = session('user');
-      $user = resultset('User')->find( $user->{id} );
-      my $status;
-      if ($user) {
-        $status = ( $user->is_admin || $user->id == $post->user->id ) ? 'approved' : 'pending';
-      }
-      else {
-        $status = 'pending';
-      }
-
-      # Filter the input data
-      $fullname =~ s/[^a-zA-Z\d\s:]//g;
-      $text     =~ s/[^a-zA-Z\d\s:]//g;
-      $email    =~ s/[^a-zA-Z\d\s:]//g;
-      $website  =~ s/[^a-zA-Z\d\s:]//g;
-
-      my $comment = resultset('Comment')->create({
-          fullname     => $fullname,
-          content      => $text,
-          email        => $email,
-          website      => $website,
-          avatar       => $gravatar,
-          post_id      => $post_id,
-          status       => $status,
-          comment_date => join ' ', $dt->ymd, $dt->hms
-        });
+      my $user    = session('user');
+      my $comment = resultset('Comment')->can_create( $params, $user );
 
       # Notify the author that a new comment was submited
       my $author = $post->user;
@@ -244,50 +206,22 @@ post '/comment/add' => sub {
     @comments = resultset('Comment')->search({ post_id => $post_id, status => 'approved' });
 
     error $@ if ( $@ );
-    
-    template 'post', 
-      { 
-        post        => $post, 
-        categories  => \@categories, 
-        comments    => \@comments,       
-        success     => 'Your comment has been submited and it will be displayed as soon as the author accepts it. Thank you!'
-      }, 
-      { layout => 'main' };
+
+    delete $template_params->{warning};
+    $template_params->{success} = 'Your comment has been submited and it will be displayed as soon as the author accepts it. Thank you!';
+    $template_params->{comments} = \@comments;
   }
   else {
     # The secret code inncorrect
     # Repopulate the fields with the data
+   
+    $template_params->{fields} = $params;
+  }
 
-    # Generate a new captcha code
-    my $captcha = Authen::Captcha->new();
+  # Store the encrypted code on the session
+  session secret => PearlBee::Helpers::Captcha::generate();
 
-    # set the data_folder. contains flatfile db to maintain state
-    $captcha->data_folder('public/captcha');
-
-    # set directory to hold publicly accessable images
-    $captcha->output_folder('public/captcha/image');
-    my $md5sum = $captcha->generate_code(5);
-
-    # Rename the file so that the encrypted code won't show on the UI
-    my $command = "mv public/captcha/image/" . $md5sum . ".png" . " public/captcha/image/image.png";
-    `$command`;
-
-    # Store the encrypted code on the session
-    session secret => $md5sum;
-
-    template 'post', 
-      { 
-        post        => $post, 
-        categories  => \@categories, 
-        comments    => \@comments,
-        fullname    => $fullname,
-        email       => $email,
-        website     => $website,
-        text        => $text,
-        warning     => 'Wrong secret code. Please enter the code again'
-      }, 
-      { layout => 'main' };
-  }  
+  template 'post', $template_params, { layout => 'main' };
   
 };
 

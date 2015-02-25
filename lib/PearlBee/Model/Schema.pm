e19c1bcb5d68311e39744eae0de9ab3bcdabaa7d
Florin Mesaros <florin.mesaros@evozon.com>
2015-02-25 15:07:19 +0200

    new version for Schema

--- lib/PearlBee/Model/Schema.pm
+++ lib/PearlBee/Model/Schema.pm
@@ -1,3 +1,4 @@
+use utf8;
 package PearlBee::Model::Schema;
 
 # Created by DBIx::Class::Schema::Loader
@@ -11,8 +12,8 @@ use base 'DBIx::Class::Schema';
 __PACKAGE__->load_namespaces;
 
 
-# Created by DBIx::Class::Schema::Loader v0.07010 @ 2014-02-07 19:20:20
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:lA3k/Z+HHUf3DevjyIQ8Kg
+# Created by DBIx::Class::Schema::Loader v0.07039 @ 2015-02-23 16:54:04
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:JjSNFVtFx/myDsjjQHMaZQ
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration

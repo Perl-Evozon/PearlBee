45ebd2ba1fec7f4a5f22dde587ca01821d4df9ca
Koble Istvan <kobleistvan@gmail.com>
2015-12-16 18:50:14 +0200

    Killed warnings and work in progress on comments

--- lib/PearlBee.pm
+++ lib/PearlBee.pm
@@ -7,7 +7,7 @@ use Dancer2::Plugin::reCAPTCHA;
 
 # Other used modules
 use DateTime;
-use JSON;
+use JSON qw//;
 
 # Included controllers
 
@@ -188,103 +188,63 @@ Add a comment method
 
 =cut
 
-post '/comment/add' => sub {
+post '/comments' => sub {
 
   my $parameters  = body_parameters;
-  my $fullname    = $parameters->{'fullname'};
   my $post_id     = $parameters->{'id'};
-  my $secret      = $parameters->{'secret'};
-  my @comments    = resultset('Comment')->search({ post_id => $post_id, status => 'approved', reply_to => undef });
+  my @comments    = resultset('Comment')->get_approved_comments_by_post_id($post_id);
   my $post        = resultset('Post')->find( $post_id );
   my @categories  = resultset('Category')->all();
-  my @recent      = resultset('Post')->search({ status => 'published' },{ order_by => { -desc => "created_date" }, rows => 3 });
+  my @recent      = resultset('Post')->get_recent_posts();
   my @popular     = resultset('View::PopularPosts')->search({}, { rows => 3 });
   my $user        = session('user');
+  my %result;
 
-  $parameters->{'reply_to'} = $1 if ($parameters->{'in_reply_to'} =~ /(\d+)/);
-  if ($parameters->{'reply_to'}) {
-    my $comm = resultset('Comment')->find({ id => $parameters->{'reply_to'} });
-    if ($comm) {
-      $parameters->{'reply_to_content'} = $comm->content;
-      $parameters->{'reply_to_user'} = $comm->fullname;
-    }
-  }
+  eval {
+    # If the person who leaves the comment is either the author or the admin the comment is automaticaly approved
 
-  my $template_params = {
-    post        => $post,
-    categories  => \@categories,
-    popular     => \@popular,
-    recent      => \@recent,
-    warning     => 'The secret code is incorrect'
-  };
+    my $comment = resultset('Comment')->can_create( $parameters, $user );
 
-  my $response = param('g-recaptcha-response');
-  my $result = recaptcha_verify($response);
+    # Notify the author that a new comment was submited
+    my $author = $post->user;
 
-  if ( $result->{success} || $ENV{CAPTCHA_BYPASS} ) {
+    # Email::Template->send( config->{email_templates} . 'new_comment.tt',
+    # {
+    #     From    => config->{default_email_sender},
+    #     To      => $author->email,
+    #     Subject => 'A new comment was submitted to your post',
 
-    # The user entered the correct secret code
-    eval {
-
-      # If the person who leaves the comment is either the author or the admin the comment is automaticaly approved
-
-      my $comment = resultset('Comment')->can_create( $parameters, $user );
-
-      # Notify the author that a new comment was submited
-      my $author = $post->user;
-
-      Email::Template->send( config->{email_templates} . 'new_comment.tt',
-      {
-          From    => config->{default_email_sender},
-          To      => $author->email,
-          Subject => ($parameters->{'reply_to'} ? 'A comment reply was submitted to your post' : 'A new comment was submitted to your post'),
-
-          tt_vars => {
-            fullname         => $fullname,
-            title            => $post->title,
-            comment          => $parameters->{'comment'},
-            signature        => config->{email_signature},
-            post_url         => config->{app_url} . '/post/' . $post->slug,
-            app_url          => config->{app_url},
-            reply_to_content => $parameters->{'reply_to_content'} || '',
-            reply_to_user    => $parameters->{'reply_to_user'}    || '',
-          },
-      }) or error "Could not send the email";
-    };
-    error $@ if ( $@ );
-
-    # Grab the approved comments for this post
-    @comments = resultset('Comment')->search({ post_id => $post->id, status => 'approved', reply_to => undef }) if ( $post );
-
-    delete $template_params->{warning};
-    delete $template_params->{in_reply_to};
+    #     tt_vars => {
+    #       fullname         => 'get fullname from signed-in commenter',
+    #       title            => $post->title,
+    #       comment          => $parameters->{'comment'},
+    #       signature        => config->{email_signature},
+    #       post_url         => config->{app_url} . '/post/' . $post->slug,
+    #       app_url          => config->{app_url}
+    #     },
+    # }) or error "Could not send the email.";
 
     if (($post->user_id && $user && $post->user_id == $user->{id}) or ($user && $user->{is_admin})) {
-      $template_params->{success} = 'Your comment has been submited. Thank you!';
+      $result{message} = 'Your comment has been submited. Thank you!';
+      $result{success} = 1;
     } else {
-      $template_params->{success} = 'Your comment has been submited and it will be displayed as soon as the author accepts it. Thank you!';
+      $result{message} = 'Your comment has been submited and it will be displayed as soon as the author accepts it. Thank you!';
+      $result{success} = 0;
     }
-  }
-  else {
-    # The secret code inncorrect
-    # Repopulate the fields with the data
+  };
 
-    $template_params->{fields} = $parameters;
-  }
+  # foreach my $comment (@comments) {
+  #   my @comment_replies = resultset('Comment')->search({ reply_to => $comment->id, status => 'approved' }, {order_by => { -asc => "comment_date" }});
+  #   foreach my $reply (@comment_replies) {
+  #     my $el;
+  #     map { $el->{$_} = $reply->$_ } ('avatar', 'fullname', 'comment_date', 'content');
+  #     $el->{uid}->{username} = $reply->uid->username if $reply->uid;
+  #     push(@{$comment->{comment_replies}}, $el);
+  #   }
+  # }
 
-  foreach my $comment (@comments) {
-    my @comment_replies = resultset('Comment')->search({ reply_to => $comment->id, status => 'approved' }, {order_by => { -asc => "comment_date" }});
-    foreach my $reply (@comment_replies) {
-      my $el;
-      map { $el->{$_} = $reply->$_ } ('avatar', 'fullname', 'comment_date', 'content');
-      $el->{uid}->{username} = $reply->uid->username if $reply->uid;
-      push(@{$comment->{comment_replies}}, $el);
-    }
-  }
-  $template_params->{comments} = \@comments;
-  $template_params->{recaptcha} = recaptcha_display();
-
-  template 'post', $template_params;
+  content_type 'application/json';
+  return to_json(\%result);
 
 };
 

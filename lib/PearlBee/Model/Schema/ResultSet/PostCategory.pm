a5ecead459adc8e6e9738c505fdb062d66dd26d8
Alexandru Strajeriu <alexandru.strajeriu@evozon.com>
2015-12-18 12:55:09 +0200

    BLOGSPERL-100 Categories and Tags

--- lib/PearlBee/Model/Schema/ResultSet/PostCategory.pm
+++ lib/PearlBee/Model/Schema/ResultSet/PostCategory.pm
@@ -6,6 +6,7 @@ use warnings;
 use base 'DBIx::Class::ResultSet';
 use String::Util qw(trim);
 use PearlBee::Helpers::Util qw/string_to_slug/;
+
 =head
 
 Associate categories with the new post
@@ -13,20 +14,30 @@ Associate categories with the new post
 =cut
 
 sub connect_categories {
-	my ($self, $categories, $post_id) = @_;
+    my ( $self, $categories, $post_id, $user_id ) = @_;
 
-	$categories      	  ||= 1;                                             					 # If no category is selected the Uncategorized category will be stored default
-    my @categories_selected = ref( $categories ) eq 'ARRAY' ? @{ $categories } : $categories;    # Force an array if only one category was selected
+    $categories ||= 'Uncategorized';    # If no category is selected the Uncategorized category will be stored default
+
+#    my @categories_selected = ref( $categories ) eq 'ARRAY' ? @{ $categories } : $categories;    # Force an array if only one category was selected
+    my @categories_selected = split( ',', $categories );
 
     # Delete any existing assocations [ For update ]
-    my @post_categories = $self->search({ post_id => $post_id });
+    my @post_categories = $self->search( {post_id => $post_id} );
     $_->delete foreach (@post_categories);
-    
-	$self->create(
-	{
-	      category_id => $_,
-	      post_id     => $post_id
-	}) foreach ( @categories_selected );
+
+    foreach my $category (@categories_selected) {
+        my $slug = string_to_slug($category);
+
+        my $db_category = $self->result_source->schema->resultset('Category')
+            ->find_or_create( {name => trim($category), slug => $slug, user_id => $user_id} );
+
+        $self->create(
+            {   category_id => $db_category->id,
+                post_id     => $post_id
+            }
+        );
+    }
+
 }
 
 =head
@@ -36,25 +47,27 @@ Used in import
 =cut
 
 sub bind_categories {
-	my ($self, $categories, $post_id, $user_id ) = @_;
+    my ( $self, $categories, $post_id, $user_id ) = @_;
 
-	$categories      	  ||= 1;                                             					 # If no category is selected the Uncategorized category will be stored default
-    my @categories_selected = ref( $categories ) eq 'ARRAY' ? @{ $categories } : $categories;    # Force an array if only one category was selected
+    $categories ||= 1;    # If no category is selected the Uncategorized category will be stored default
+    my @categories_selected =
+        ref($categories) eq 'ARRAY' ? @{$categories} : $categories;   # Force an array if only one category was selected
 
     # Delete any existing assocations [ For update ]
-    my @post_categories = $self->search({ post_id => $post_id });
+    my @post_categories = $self->search( {post_id => $post_id} );
     $_->delete foreach (@post_categories);
-    
+
     foreach my $category (@categories_selected) {
-        my $slug = string_to_slug( $category );
-        
-        my $db_category = $self->result_source->schema->resultset('Category')->find_or_create({ name => trim($category), slug => $slug, user_id => $user_id });
-        
+        my $slug = string_to_slug($category);
+
+        my $db_category = $self->result_source->schema->resultset('Category')
+            ->find_or_create( {name => trim($category), slug => $slug, user_id => $user_id} );
+
         $self->create(
-        {
-            category_id => $db_category->id,
-            post_id     => $post_id
-        });
+            {   category_id => $db_category->id,
+                post_id     => $post_id
+            }
+        );
     }
 }
 

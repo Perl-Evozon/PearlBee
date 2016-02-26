6ffc2833ee6b7155089d98e85452636a4e5da0ce
Paul Mercean <paul.mercean@evozon.com>
2016-02-26 16:15:23 +0000

    catgory title link change - resset pass changed - log in without Dan's changes

--- public/blog/js/app.js
+++ public/blog/js/app.js
@@ -394,7 +394,7 @@ function getUserPosts(searchTerm, pageNumber, removeExistingPosts) {
         if (true === removeExistingPosts) {
             $('#tab-content1 .progressloader-holder').show();
         }
-        var themeinitial = $('#cmn-toggle-4').is(':checked');
+
         $('.progressloader').show();
         $.ajax({
             // Assuming an endpoint here that responds to GETs with a response.
@@ -426,8 +426,7 @@ function getUserPosts(searchTerm, pageNumber, removeExistingPosts) {
                     for (var i = 0; i < posts.length; i++) {
                         var entryItem = $(".entry").get(0),
                             newItem = $(entryItem).clone(),
-                            commentsText,
-                            avatarPath;
+                            commentsText;
 
                         if (posts[i].nr_of_comments == 1) {
                             commentsText = "Comment";
@@ -435,21 +434,10 @@ function getUserPosts(searchTerm, pageNumber, removeExistingPosts) {
                             commentsText = "Comments (" + posts[i].nr_of_comments + ")";
                         }
 
-                        if (posts[i].user.avatar) {
-                            avatarPath = posts[i].user.avatar;
-                            } else if ( themeinitial === false) {
-                           avatarPath = "/blog/img/male-user.png";
-                            } else if ( themeinitial === true) {
-                            avatarPath = "/blog/img/male-user-light.png";
-                            }
 
-                       // console.log("avatarPath",avatarPath);
-
-
-                        newItem.find(".bubble img.user-image").attr("src", avatarPath);
                         newItem.find(".user a").html(posts[i].username);
                         newItem.find(".user a").attr("href", "/profile/author/" + posts[i].username);
-                        newItem.find(".post_preview_wrapper").html(posts[i].content.replace(/<\/?[^>]+(>|$)/g, ""));
+                        newItem.find(".post_preview_wrapper").html(posts[i].content);
                         newItem.find(".post-heading h2 a").attr("href", "/post/" + posts[i].slug);
                         newItem.find(".post-heading h2 a").html(posts[i].title);
                         newItem.find(".comments-listings a").text(commentsText);
@@ -681,7 +669,7 @@ if (newURL == userURL) {
                     }
 
                     newItem.find(".user a").attr("href", "/posts/user/" + posts[i].user.username);
-                    newItem.find(".post_preview_wrapper").html(posts[i].content.replace(/<\/?[^>]+(>|$)/g, ""));
+                    newItem.find(".post_preview_wrapper").html(posts[i].content);
                     newItem.find(".post-heading h2 a").attr("href", "/post/" + posts[i].title);
                     newItem.find(".user a").html(posts[i].user.username);
                     newItem.find(".post-heading h2 a").html(posts[i].title);
@@ -757,7 +745,7 @@ if (newURL == userURL) {
                     }
 
                     newItem.find(".user a").attr("href", "/profile/author/" + posts[i].user.username);
-                    newItem.find(".post_preview_wrapper").html(posts[i].content.replace(/<\/?[^>]+(>|$)/g, ""));
+                    newItem.find(".post_preview_wrapper").html(posts[i].content);
                     newItem.find(".post-heading h2 a").attr("href", "/post/" + posts[i].title);
                     newItem.find(".user a").html(posts[i].user.username);
                     newItem.find(".post-heading h2 a").html(posts[i].title);
@@ -833,8 +821,8 @@ if (newURL == userURL) {
                     }
 
                     newItem.find(".user a").attr("href", "/posts/user/" + posts[i].user.username);
-                    newItem.find(".post_preview_wrapper").html(posts[i].content.replace(/<\/?[^>]+(>|$)/g, ""));
-                    newItem.find(".post-heading h2 a").attr("href", "/post/" + posts[i].title);
+                    newItem.find(".post_preview_wrapper").html(posts[i].content);
+                    newItem.find(".post-heading h2 a").attr("href", "/post/" + posts[i].slug);
                     newItem.find(".user a").html(posts[i].user.username);
                     newItem.find(".post-heading h2 a").html(posts[i].title);
                     newItem.find(".comments-listings a").text(commentsText);
@@ -899,7 +887,7 @@ $('#more-posts').click(function() {
                 }
 
                 newItem.find(".user a").attr("href", "/profile/author/" + posts[i].user.username);
-                newItem.find(".post_preview_wrapper").html(posts[i].content.replace(/<\/?[^>]+(>|$)/g, ""));
+                newItem.find(".post_preview_wrapper").html(posts[i].content);
                 newItem.find(".post-heading h2 a").attr("href", "/post/" + posts[i].title);
                 newItem.find(".user a").html(posts[i].user.username);
                 newItem.find(".post-heading h2 a").html(posts[i].title);
@@ -1005,7 +993,7 @@ function getAuthorEntries (button) {
                     }
 
                     newItem.find(".user a").attr("href", "/profile/author/" + posts[i].user.username);
-                    newItem.find(".post_preview_wrapper").html(posts[i].content.replace(/<\/?[^>]+(>|$)/g, ""));
+                    newItem.find(".post_preview_wrapper").html(posts[i].content);
                     newItem.find(".post-heading h2 a").attr("href", "/post/" + posts[i].title);
                     newItem.find(".user a").html(posts[i].user.username);
                     newItem.find(".post-heading h2 a").html(posts[i].title);

70d15b0a78c9cc90ba18fd4cbb3bafc59d898160
Dan Fumarel <dan.fumarel@evozon.com>
2016-03-03 13:57:06 +0000

    fix conflicts with paul/master

--- public/blog/js/app.js
+++ public/blog/js/app.js
@@ -337,6 +337,30 @@ $("#start-blogging").on('click', function (e) {
     }
   });
 
+
+//My Profile PAGE - Change Picture
+ $(document).on('change', '.btn-file :file', function() {
+   var input = $(this),
+       numFiles = input.get(0).files ? input.get(0).files.length : 1,
+       label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+   input.trigger('fileselect', [numFiles, label]);
+ });
+ 
+ $(document).ready( function() {
+     $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+         
+         var input = $(this).parents('.input-group').find(':text'),
+             log = numFiles > 1 ? numFiles + ' files selected' : label;
+         
+         if( input.length ) {
+             input.val(log);
+         } else {
+             if( log ) alert(log);
+         }
+         
+     });
+ });
+
 //  Sign up
     $('.sign-up').css('min-height',$(window).height()-80);
     $(window).resize(function(){
@@ -349,10 +373,10 @@ $("#start-blogging").on('click', function (e) {
       $('.register').css('min-height',$(window).height()-80);
     });
 //Listing page
-//	$('.blog').css('min-height', $(window).height()-$('footer').height()-45);
-//	$(window).resize(function(){
-//		$('.blog').css('min-height', $(window).height()-$('footer').height()-45);
-//	}); 
+	$('.blog').css('min-height', $(window).height()-$('footer').height()-45);
+	$(window).resize(function(){
+		$('.blog').css('min-height', $(window).height()-$('footer').height()-45);
+	}); 
 //Individual posts 
 	$('.blog-post').css('min-height', $(window).height()-$('.blog-comment').height()-$('footer').height()-80);
 	$(window).resize(function(){

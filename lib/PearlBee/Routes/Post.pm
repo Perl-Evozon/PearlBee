ace14484ef38bb0d384550bbe32deed9746ed84a
Jeffrey Goff <jeffrey.goff@evozon.com>
2016-04-05 12:05:35 +0300

    BLOGSPERL-327 Display the tag name rather than slug

--- lib/PearlBee/Routes/Post.pm
+++ lib/PearlBee/Routes/Post.pm
@@ -61,45 +61,51 @@ get '/post/:slug' => sub {
 
 get '/posts/category/:slug' => sub {
 
-  my $slug         = route_parameters->{'slug'};
-  my $category_obj = resultset('Category')->find({ 'slug' => $slug });
-  my $nr_of_rows   = config->{posts_on_page} || 5; # Number of posts per page
-  my $page         = 1;
-  my @posts        = resultset('Post')->search_published({ 'category.slug' => $slug }, { join => { 'post_categories' => 'category' }, order_by => { -desc => "created_date" }, rows => $nr_of_rows, page => $page });
-  unless ( @posts ) {
-    error "Could not find posts for slug '$slug'";
+  my $slug     = route_parameters->{'slug'};
+  my $category = resultset('Category')->find({ 'slug' => $slug });
+
+  my $template_data;
+  if ( $category ) {
+    my $nr_of_rows   = config->{posts_on_page} || 5; # Number of posts per page
+    my $page         = 1;
+    my @posts        = resultset('Post')->search_published({ 'category.slug' => $slug }, { join => { 'post_categories' => 'category' }, order_by => { -desc => "created_date" }, rows => $nr_of_rows, page => $page });
+    unless ( @posts ) {
+      error "Could not find posts for slug '$slug'";
+    }
+    my $total_posts = resultset('Post')->search_published({ 'category.slug' => $slug }, { join => { 'post_categories' => 'category' } })->count;
+    my @recent      = map { $_->as_hashref_sanitized }
+                      resultset('Post')->search_published({},{ order_by => { -desc => "created_date" }, rows => 3 });
+    my @popular     = map { $_->as_hashref_sanitized }
+                      resultset('View::PopularPosts')->search({}, { rows => 3 });
+    my @tags        = map { $_->as_hashref_sanitized }
+                      map { $_->tag_objects } @posts;
+    my @categories  = map { $_->as_hashref_sanitized }
+                      map { $_->category_objects } @posts;
+ 
+    # extract demo posts info
+    my @mapped_posts = map_posts(@posts);
+ 
+    # Calculate the next and previous page link
+    my $total_pages                 = get_total_pages($total_posts, $nr_of_rows);
+    my ($previous_link, $next_link) = get_previous_next_link($page, $total_pages, '/posts/category/' . $slug);
+ 
+    # Extract all posts with the wanted category
+    $template_data = {
+      posts         => \@mapped_posts,
+      recent        => \@recent,
+      popular       => \@popular,
+      tags          => \@tags,
+      page          => $page,
+      categories    => \@categories,
+      total_pages   => $total_pages,
+      next_link     => $next_link,
+      previous_link => $previous_link,
+      category      => $category->as_hashref_sanitized
+    };
   }
-  my $total_posts = resultset('Post')->search_published({ 'category.slug' => $slug }, { join => { 'post_categories' => 'category' } })->count;
-  my @recent      = map { $_->as_hashref_sanitized }
-                    resultset('Post')->search_published({},{ order_by => { -desc => "created_date" }, rows => 3 });
-  my @popular     = map { $_->as_hashref_sanitized }
-                    resultset('View::PopularPosts')->search({}, { rows => 3 });
-  my @tags        = map { $_->as_hashref_sanitized }
-                    map { $_->tag_objects } @posts;
-  my @categories  = map { $_->as_hashref_sanitized }
-                    map { $_->category_objects } @posts;
-
-  # extract demo posts info
-  my @mapped_posts = map_posts(@posts);
-
-  # Calculate the next and previous page link
-  my $total_pages                 = get_total_pages($total_posts, $nr_of_rows);
-  my ($previous_link, $next_link) = get_previous_next_link($page, $total_pages, '/posts/category/' . $slug);
-
-  # Extract all posts with the wanted category
-  my $template_data = {
-    posts              => \@mapped_posts,
-    recent             => \@recent,
-    popular            => \@popular,
-    tags               => \@tags,
-    page               => $page,
-    categories         => \@categories,
-    total_pages        => $total_pages,
-    next_link          => $next_link,
-    previous_link      => $previous_link,
-    posts_for_category => $slug,
-    category_name      => $category_obj->name,
-  };
+  else {
+    error "No category found for slug '$slug'";
+  }
 
   if ( param('format') ) {
     my $json = JSON->new;
@@ -112,7 +118,7 @@ get '/posts/category/:slug' => sub {
   }
 };
 
-=head2 View posts by category
+=head2 View pages by category
 
 =cut
 
@@ -121,44 +127,52 @@ get '/posts/category/:slug/page/:page' => sub {
   my $nr_of_rows  = config->{posts_on_page} || 10; # Number of posts per page
   my $page        = route_parameters->{'page'};
   my $slug        = route_parameters->{'slug'};
-  my @posts       = resultset('Post')->search_published({ 'category.slug' => $slug }, { join => { 'post_categories' => 'category' }, order_by => { -desc => "created_date" }, rows => $nr_of_rows, page => $page });
-  my $nr_of_posts = resultset('Post')->search_published({ 'category.slug' => $slug }, { join => { 'post_categories' => 'category' } })->count;
-  my @tags        = map { $_->as_hashref_sanitized }
-                    map { $_->tag_objects } @posts;
-  my @categories  = map { $_->as_hashref_sanitized }
-                    map { $_->category_objects } @posts;
-  my @recent      = map { $_->as_hashref_sanitized }
-                    resultset('Post')->search_published({},{ order_by => { -desc => "created_date" }, rows => 3 });
-  my @popular     = map { $_->as_hashref_sanitized }
-                    resultset('View::PopularPosts')->search({}, { rows => 3 });
+  my $category    = resultset('Category')->find({ 'slug' => $slug });
+  my $template_data;
 
-  # extract demo posts info
-  my @mapped_posts = map_posts(@posts);
-  my $movable_type_url = config->{movable_type_url};
-  my $app_url = config->{app_url};
-
-  for my $post ( @mapped_posts ) {
-    $post->{content} =~ s{$movable_type_url}{$app_url}g;
+  if ( $category ) {
+    my @posts       = resultset('Post')->search_published({ 'category.slug' => $slug }, { join => { 'post_categories' => 'category' }, order_by => { -desc => "created_date" }, rows => $nr_of_rows, page => $page });
+    my $nr_of_posts = resultset('Post')->search_published({ 'category.slug' => $slug }, { join => { 'post_categories' => 'category' } })->count;
+    my @tags        = map { $_->as_hashref_sanitized }
+                      map { $_->tag_objects } @posts;
+    my @categories  = map { $_->as_hashref_sanitized }
+                      map { $_->category_objects } @posts;
+    my @recent      = map { $_->as_hashref_sanitized }
+                      resultset('Post')->search_published({},{ order_by => { -desc => "created_date" }, rows => 3 });
+    my @popular     = map { $_->as_hashref_sanitized }
+                      resultset('View::PopularPosts')->search({}, { rows => 3 });
+ 
+    # extract demo posts info
+    my @mapped_posts = map_posts(@posts);
+    my $movable_type_url = config->{movable_type_url};
+    my $app_url = config->{app_url};
+ 
+    for my $post ( @mapped_posts ) {
+      $post->{content} =~ s{$movable_type_url}{$app_url}g;
+    }
+ 
+    # Calculate the next and previous page link
+    my $total_pages                 = get_total_pages($nr_of_posts, $nr_of_rows);
+    my ($previous_link, $next_link) = get_previous_next_link($page, $total_pages, '/posts/category/' . $slug);
+ 
+    # Extract all posts with the wanted category
+    $template_data =
+      {
+      posts         => \@mapped_posts,
+      recent        => \@recent,
+      popular       => \@popular,
+      tags          => \@tags,
+      categories    => \@categories,
+      page          => $page,
+      total_pages   => $total_pages,
+      next_link     => $next_link,
+      previous_link => $previous_link,
+      category      => $category->as_hashref_serialized
+      };
   }
-
-  # Calculate the next and previous page link
-  my $total_pages                 = get_total_pages($nr_of_posts, $nr_of_rows);
-  my ($previous_link, $next_link) = get_previous_next_link($page, $total_pages, '/posts/category/' . $slug);
-
-  # Extract all posts with the wanted category
-  my $template_data =
-    {
-    posts              => \@mapped_posts,
-    recent             => \@recent,
-    popular            => \@popular,
-    tags               => \@tags,
-    categories         => \@categories,
-    page               => $page,
-    total_pages        => $total_pages,
-    next_link          => $next_link,
-    previous_link      => $previous_link,
-    posts_for_category => $slug
-    };
+  else {
+    error "No category found for slug '$slug'";
+  }
 
   if ( param('format') ) {
     my $json = JSON->new;
@@ -333,38 +347,56 @@ get '/posts/user/:username/page/:page' => sub {
 
 get '/posts/tag/:slug' => sub {
 
-  my $nr_of_rows  = config->{posts_on_page} || 5; # Number of posts per page
   my $slug        = route_parameters->{'slug'};
-  my @posts       = resultset('Post')->search_published({ 'tag.slug' => $slug }, { join => { 'post_tags' => 'tag' }, order_by => { -desc => "created_date" }, rows => $nr_of_rows });
-  my $nr_of_posts = resultset('Post')->search_published({ 'tag.slug' => $slug }, { join => { 'post_tags' => 'tag' } })->count;
-  my @recent      = map { $_->as_hashref_sanitized }
-                    resultset('Post')->search_published({},{ order_by => { -desc => "created_date" }, rows => 3 });
-  my @popular     = map { $_->as_hashref_sanitized }
-                    resultset('View::PopularPosts')->search({}, { rows => 3 });
-  my @tags        = map { $_->as_hashref_sanitized }
-                    map { $_->tag_objects } @posts;
-  my @categories  = map { $_->as_hashref_sanitized }
-                    map { $_->category_objects } @posts;
+  my $tag         = resultset('Tag')->find({ slug => $slug });
+  my $template_data;
 
-  # extract demo posts info
-  my @mapped_posts = map_posts(@posts);
+  if ( $tag ) {
+    my $nr_of_rows  = config->{posts_on_page} || 5; # Number of posts per page
+    my @posts = resultset('Post')->
+                  search_published({ 'tag.slug' => $slug },
+                                   { join => { 'post_tags' => 'tag' },
+                                     order_by => { -desc => "created_date" },
+                                     rows => $nr_of_rows });
+    my $nr_of_posts = resultset('Post')->
+                        search_published({ 'tag.slug' => $slug },
+                                         { join => { 'post_tags' => 'tag' } })->                          count;
+    my @recent      = map { $_->as_hashref_sanitized }
+                      resultset('Post')->
+                        search_published({}, { order_by =>
+                                               { -desc => "created_date" },
+                                               rows => 3 });
+    my @popular     = map { $_->as_hashref_sanitized }
+                      resultset('View::PopularPosts')->
+                        search({}, { rows => 3 });
+    my @tags        = map { $_->as_hashref_sanitized }
+                      map { $_->tag_objects } @posts;
+    my @categories  = map { $_->as_hashref_sanitized }
+                      map { $_->category_objects } @posts;
 
-  # Calculate the next and previous page link
-  my $total_pages                 = get_total_pages($nr_of_posts, $nr_of_rows);
-  my ($previous_link, $next_link) = get_previous_next_link(1, $total_pages, '/posts/tag/' . $slug);
+    # extract demo posts info
+    my @mapped_posts = map_posts(@posts);
 
-  my $template_data = {
-    posts         => \@mapped_posts,
-    recent        => \@recent,
-    popular       => \@popular,
-    tags          => \@tags,
-    page          => 1,
-    categories    => \@categories,
-    total_pages   => $total_pages,
-    next_link     => $next_link,
-    previous_link => $previous_link,
-    posts_for_tag => $slug
-  };
+    # Calculate the next and previous page link
+    my $total_pages                 = get_total_pages($nr_of_posts, $nr_of_rows);
+    my ($previous_link, $next_link) = get_previous_next_link(1, $total_pages, '/posts/tag/' . $slug);
+
+    $template_data = {
+      posts         => \@mapped_posts,
+      recent        => \@recent,
+      popular       => \@popular,
+      tags          => \@tags,
+      page          => 1,
+      categories    => \@categories,
+      total_pages   => $total_pages,
+      next_link     => $next_link,
+      previous_link => $previous_link,
+      tag           => $tag->as_hashref_sanitized
+    };
+  }
+  else {
+    error "Could not find tag for slug '$slug'";
+  }
 
   if ( param('format') ) {
     my $json = JSON->new;

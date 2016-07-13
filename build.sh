c382c9370c52c78c708a1c293de0e2f9aa9eef08
Justin Wheeler <jwheeler@datademons.com>
2016-07-13 03:18:52 -0400

    Add --missing to dzil call.
    Only install modules that are missing.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 $(which cpanm >/dev/null 2>&1) || ( curl -L http://cpanmin.us | perl - App::cpanminus )
 $(which dzil >/dev/null 2>&1) || perl $(which cpanm) Dist::Zilla
-dzil listdeps | xargs cpanm
+dzil listdeps --missing | xargs cpanm

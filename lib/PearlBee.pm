7b27fe3b1d9187d78c71579a9b153fbfe1099c96
Dragos Trif <drd.trif@gmail.com>
2016-07-26 18:56:28 +0300

    comments

--- lib/PearlBee.pm
+++ lib/PearlBee.pm
@@ -215,7 +215,7 @@ post '/comment/add' => sub {
 
   my $response = param('g-recaptcha-response');
   warn "The response is |$response |";
-  #my ${$result} = recaptcha_verify($response);
+  my {$result} = recaptcha_verify($response);
   warn "The response in english is:\n ";
   warn recaptcha_verify($response);
 

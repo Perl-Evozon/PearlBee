0795c5ad68a3a99aa70ff22273021fa926604b33
Dragos Trif <drd.trif@gmail.com>
2016-07-26 13:27:14 +0300

    comments

--- lib/PearlBee.pm
+++ lib/PearlBee.pm
@@ -177,10 +177,15 @@ Add a comment method
 
 post '/comment/add' => sub {
 
+   my $response = param('g-recaptcha-response');
+  warn "The response is |$response |";
+  my $result = recaptcha_verify($response);
+  #warn "The response in englidh is |$result->{success} |";
+
   my $parameters  = body_parameters;
   my $fullname    = $parameters->{'fullname'};
   my $post_id     = $parameters->{'id'};
-  my $secret      = param('g-recaptcha-response');#$parameters->{'secret'};
+  my $secret      = $result;#param('g-recaptcha-response');#$parameters->{'secret'};
   my @comments    = resultset('Comment')->search({ post_id => $post_id, status => 'approved', reply_to => undef });
   my $post        = resultset('Post')->find( $post_id );
   my @categories  = resultset('Category')->all();
@@ -207,10 +212,7 @@ post '/comment/add' => sub {
 
   };
 
-  my $response = param('g-recaptcha-response');
-  warn "The response is |$response |";
-  my $result = recaptcha_verify($response);
-  warn "The response in englidh is |$result->{success} |";
+ 
 
 
   if ( $result->{success} ) {

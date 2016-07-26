cb66deac40ec6c09bce30200f06f7e3679ada0d7
Dragos Trif <drd.trif@gmail.com>
2016-07-26 15:05:24 +0300

    comments

--- lib/PearlBee.pm
+++ lib/PearlBee.pm
@@ -186,7 +186,7 @@ post '/comment/add' => sub {
   my $parameters  = body_parameters;
   my $fullname    = $parameters->{'fullname'};
   my $post_id     = $parameters->{'id'};
-  my $secret      = $result;#$parameters->{'$result'};#$parameters->{'secret'};
+  #my $secret      = $result;#$parameters->{'$result'};#$parameters->{'secret'};
   my @comments    = resultset('Comment')->search({ post_id => $post_id, status => 'approved', reply_to => undef });
   my $post        = resultset('Post')->find( $post_id );
   my @categories  = resultset('Category')->all();

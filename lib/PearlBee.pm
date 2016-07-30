0b14869d9014f115b39e172d16fbec8ac5258777
Dragos Trif <drd.trif@gmail.com>
2016-07-30 16:55:22 +0300

    cooment

--- lib/PearlBee.pm
+++ lib/PearlBee.pm
@@ -278,7 +278,7 @@ post '/comment/add' => sub {
     #my $response = param('g-recaptcha-response');
     #template 'comment_form', { recaptcha => recaptcha_display() };
     #fallback;
-    my $post_title = $post->'title';
+    my $post_title = $post->title;
     warn $post_title;
     redirect  '/post/:post_title';
   }

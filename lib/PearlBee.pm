32b41cad682db562167d342ba4589851420ea0b5
Dragos Trif <drd.trif@gmail.com>
2016-07-31 10:42:46 +0300

    cooment

--- lib/PearlBee.pm
+++ lib/PearlBee.pm
@@ -184,7 +184,7 @@ post '/comment/add' => sub {
   my $parameters  = body_parameters;
   my $fullname    = $parameters->{'fullname'};
   my $post_id     = $parameters->{'id'};
-  my $secret      = $parameters->{'secret'};
+  my $secret      = $parameters->{recaptcha_display()};
   my @comments    = resultset('Comment')->search({ post_id => $post_id, status => 'approved', reply_to => undef });
   my $post        = resultset('Post')->find( $post_id );
   my @categories  = resultset('Category')->all();
